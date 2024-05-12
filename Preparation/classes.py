37f3726703d7977dc79b97d612daae6fb2f8472c
Divya Tiwari <gungun130903@gmail.com>
2024-05-12 12:35:16 +0530

    Class hierarchies

---
+++ Preparation/classes.py
@@ -0,0 +1,57 @@
+"""Understanding class hierarchy in Python."""
+
+import math
+
+class Circles:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return round(math.pi*(self.radius**2),2)
+    
+
+class Car:
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        self.started = False
+        self.speed = 0
+        self.max_speed = 200
+
+    def start(self):
+        print("Starting the car...")
+        self.started = True
+
+    def stop(self):
+        print("Stopping the car...")
+        self.started = False
+
+    def accelerate(self, value):
+        if not self.started:
+            print("Car is not started!")
+            return
+        if self.speed + value <= self.max_speed:
+            self.speed += value
+        else:
+            self.speed = self.max_speed
+        print(f"Accelerating to {self.speed} km/h...")
+
+    def brake(self, value):
+        if self.speed - value >= 0:
+            self.speed -= value
+        else:
+            self.speed = 0
+        print(f"Braking to {self.speed} km/h...")
+
+
+class SampleClass:
+    class_attr = 100
+
+    def __init__(self, instance_attr):
+        self.instance_attr = instance_attr
+
+    def method(self):
+        print(f"Class attribute: {self.class_attr}")
+        print(f"Instance attribute: {self.instance_attr}")
