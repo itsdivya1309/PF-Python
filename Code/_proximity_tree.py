0975d7fec25c390ceac071f49375f6f7a0fe332e
Divya Tiwari <gungun130903@gmail.com>
2024-06-12 21:12:51 +0530

    Update

--- Code/_proximity_tree.py
+++ Code/_proximity_tree.py
@@ -1,45 +1,58 @@
 """Generate candidate splitter."""
 
 import numpy as np
+
+# from numba import njit
+
+from aeon.classifier import BaseClassifier
+
 # from sklearn.preprocessing import LabelEncoder
 
+
+# @njit(cache=True, fastmath=True)
 def get_parameter_value(X=None):
-    """For a list of distance measures, generate a dictionary 
+    """Generate random parameter values.
+
+    For a list of distance measures, generate a dictionary
     of parameterized distances.
-    
+
     Parameters
     ----------
     X : np.ndarray of shape (n_cases, n_timepoints)
-        
+
     Returns
     -------
-    distance_param : a list of distances and their 
+    distance_param : a list of distances and their
         parameters.
     """
     X_std = X.std()
     param_ranges = {
         "euclidean": {},
-        "dtw": {"window": (0,0.25)},
-        "ddtw": {"window": (0,0.25)},
-        "wdtw": {"g": (0,1)},
-        "wddtw": {"g": (0,1)},
-        "erp": {"g": (X_std/5,X_std)},
-        "lcss": {"epsilon": (X_std/5,X_std),
-                 "window": (0,0.25)}
+        "dtw": {"window": (0, 0.25)},
+        "ddtw": {"window": (0, 0.25)},
+        "wdtw": {"g": (0, 1)},
+        "wddtw": {"g": (0, 1)},
+        "erp": {"g": (X_std / 5, X_std)},
+        "lcss": {"epsilon": (X_std / 5, X_std), "window": (0, 0.25)},
     }
     random_params = []
     for measure, ranges in param_ranges.items():
-        random_params.append({measure : {param: np.round(np.random.uniform(low,high),3)
-                                  for param, (low,high) in ranges.items()}})
-        
+        random_params.append(
+            {
+                measure: {
+                    param: np.round(np.random.uniform(low, high), 3)
+                    for param, (low, high) in ranges.items()
+                }
+            }
+        )
+
     # For TWE
-    lmbda = np.random.randint(0,9)
-    exponent_range = np.arange(1,6)  # Exponents from -5 to 1 (inclusive)
+    lmbda = np.random.randint(0, 9)
+    exponent_range = np.arange(1, 6)  # Exponents from -5 to 1 (inclusive)
     random_exponent = np.random.choice(exponent_range)
-    nu = 1/10**random_exponent
-    random_params.append({ "twe" : {"lmbda": lmbda,
-                            "nu": nu}})
-    
+    nu = 1 / 10**random_exponent
+    random_params.append({"twe": {"lmbda": lmbda, "nu": nu}})
+
     # For MSM
     base = 10
     # Exponents from -2 to 2 (inclusive)
@@ -48,18 +61,19 @@ def get_parameter_value(X=None):
     # Randomly select an index from the exponent range
     random_index = np.random.randint(0, len(exponents))
     c = base ** exponents[random_index]
-    random_params.append({"msm" : {"c": c}})
-    
+    random_params.append({"msm": {"c": c}})
+
     return random_params
 
 
+# @njit(cache=True, fastmath=True)
 def get_candidate_splitter(X, y, paramterized_distances):
     """Generate candidate splitter.
-    
-    Takes a time series dataset and a set of parameterized 
-    distance measures to create a candidate splitter, which 
+
+    Takes a time series dataset and a set of parameterized
+    distance measures to create a candidate splitter, which
     contains a parameterized distance measure and a set of exemplars.
-    
+
     Parameters
     ----------
     X : np.ndarray shape (n_cases, n_timepoints)
@@ -75,27 +89,25 @@ def get_candidate_splitter(X, y, paramterized_distances):
     """
     _X = X
     _y = y
-    #label_encoder = LabelEncoder()
-    #_y_label = label_encoder.fit_transform(_y)
-    #if _y_label.ndim == 1:
+    # label_encoder = LabelEncoder()
+    # _y_label = label_encoder.fit_transform(_y)
+    # if _y_label.ndim == 1:
     #    _y_label = _y_label.reshape(-1,1)
-    #_X_y = np.concatenate([_X,_y_label], axis=1)
+    # _X_y = np.concatenate([_X,_y_label], axis=1)
 
     # Now, I need to create a dictionary
     # where the keys will be unique classes and values will be a random
     # data of that class
     exemplars = []
     for label in np.unique(_y):
-        y_new = _y[_y==label]
-        X_new = _X[_y==label]
+        y_new = _y[_y == label]
+        X_new = _X[_y == label]
         id = np.random.randint(0, X_new.shape[0])
-        exemplars.append({y_new[id] : X_new[id,:]})
+        exemplars.append({y_new[id]: X_new[id, :]})
 
-    # Create a list with first element exemplars and second element a random 
+    # Create a list with first element exemplars and second element a random
     # parameterized distance measure
-    n = np.random.randint(0,9)
+    n = np.random.randint(0, 9)
     splitter = [exemplars, paramterized_distances[n]]
 
-    
     return splitter
-
